79a7cf271c7dfd4a0d587db64e5e754dce326595
Gabriela Poncet <gabiponcet@gmail.com>
2020-03-04 15:40:00 -0300

    Initial commit

---
+++ app/src/main/java/br/com/vendas20201/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.vendas20201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
